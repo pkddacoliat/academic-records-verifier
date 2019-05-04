1b2cb7c4bb54346e0a50da5a7048718afd791552
Patrick Kent Dacoliat <pkddacoliat@gmail.com>
2019-05-04 14:01:08 +0100

    Add navigation bar and other partials

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'Express' });
+  res.render('pages/index', { title: 'Academic Records Verifier' });
 });
 
 module.exports = router;

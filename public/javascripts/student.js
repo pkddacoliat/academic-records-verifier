6f061df199d2c2f8e51ab3a3d5a9ed4c2b47993b
Patrick Kent Dacoliat <pkddacoliat@gmail.com>
2019-05-08 13:08:08 +0100

    Add modal for viewing the grade history

--- public/javascripts/student.js
+++ public/javascripts/student.js
@@ -32,7 +32,20 @@ function loadStudentData() {
     $("#studentGradesTable").DataTable({
       data: studentGrades,
       columns: [
-        { data: "gradeId" },
+        {
+          data: "gradeId",
+          render: data => {
+            return (
+              '<a href="#" onClick=viewGradeHistory("' +
+              data +
+              '") class="gradeIdBtn" data-id="' +
+              data +
+              '">' +
+              data +
+              "</a>"
+            );
+          }
+        },
         { data: "stage" },
         { data: "semester" },
         { data: "crn" },
@@ -45,4 +58,29 @@ function loadStudentData() {
     });
   });
   return studentGrades;
+}
+
+function viewGradeHistory(gradeId) {
+  let studentNo = gradeId.split("_")[0];
+  let crn = gradeId.split("_")[1];
+
+  $("#gradeHistoryModal").modal({});
+  $.ajax({
+    type: "GET",
+    url:
+      "http://localhost:3000/api/queries/selectHistoryOfGradeAssignedToStudent?studentNumber=" +
+      studentNo +
+      "&moduleCRN=" +
+      crn,
+    success: data => {
+      // console.log(data);
+    }
+  }).done(data => {
+    console.log(data);
+    $("#gradeHistoryForm #moduleCRNInput").val(data[0].moduleCRN);
+    $("#gradeHistoryForm #studentNoInput").val(data[0].studentNumber);
+    $("#gradeHistoryForm #finalGradeInput").val(data[0].finalGrade);
+    $("#gradeHistoryForm #transactionIdInput").val(data[0].transactionId);
+    $("#gradeHistoryForm #timeStampInput").val(data[0].timestamp);
+  });
 }
